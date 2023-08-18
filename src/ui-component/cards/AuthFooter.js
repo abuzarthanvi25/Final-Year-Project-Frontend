376f7173a6b6f84df01b6494ade5fb51159e9ea5
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2023-08-18 17:47:45 +0500

    First Commit

---
+++ src/ui-component/cards/AuthFooter.js
@@ -0,0 +1,14 @@
+// material-ui
+import { Link, Typography, Stack } from '@mui/material';
+
+// ==============================|| FOOTER - AUTHENTICATION 2 & 3 ||============================== //
+
+const AuthFooter = () => (
+  <Stack direction="row" justifyContent="space-between">
+    <Typography variant="subtitle2" component={Link} href="https://berrydashboard.io" target="_blank" underline="hover">
+    skillsift.ai
+    </Typography>
+  </Stack>
+);
+
+export default AuthFooter;
