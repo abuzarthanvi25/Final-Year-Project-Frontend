0812c6dc279a438e3f60fc357460e15a573d1917
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-09-06 23:06:11 +0500

    Update Logo.js

--- src/ui-component/Logo.js
+++ src/ui-component/Logo.js
@@ -2,15 +2,14 @@
 // import { useTheme } from '@mui/material/styles';
 
 // import logoDark from 'assets/images/logo-dark.svg';
-import logo from 'assets/images/logo.svg';
-
-// ==============================|| LOGO SVG ||============================== //
+import myLogo from 'assets/images/final.png'; // Update the import path to your new logo file
 
 const Logo = () => {
-  // const theme = useTheme();
-
   return (
-    <img src={logo} alt="Berry" width="100" />
+    <img src={myLogo} alt="Your Logo Alt Text" width="180"  height="75" />
+  );
+};
+export default Logo;
 
     // <svg width="92" height="32" viewBox="0 0 92 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     //   <path
